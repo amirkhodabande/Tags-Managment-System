350a33ec5c2bc0699f5da10e23f3ff6b19e90748
Amir Khodabande <amirdp8102@gmail.com>
2021-04-11 11:45:38 +0430

    Add Resource and Collection for tags

---
+++ app/Http/Resources/Tag/TagCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Tag;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TagCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'image' => $this->getFirstMedia() ? $this->getFirstMedia()->getUrl() : "",
+        ];
+    }
+}
---
+++ app/Http/Resources/Tag/TagResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Tag;
+
+use App\Models\Product;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'image' => $this->getFirstMedia() ? $this->getFirstMedia()->getUrl() : "",
+            'products' => [
+                $this->products
+            ]
+        ];
+    }
+}
