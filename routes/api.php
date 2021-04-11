aec1f80a59f311e4834e339cc380464dd4a33ea0
Amir Khodabande <amirdp8102@gmail.com>
2021-04-11 14:49:43 +0430

    Add api routes for products & articles

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/tags', tagController::class);
+Route::apiResource('/tags', TagController::class);
+
+Route::post('/products', [ProductController::class, 'store']);
+Route::post('/articles', [ArticleController::class, 'store']);
