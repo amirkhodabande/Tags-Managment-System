338d0991eed70cf3620cc1ffc4b385770810f1aa
Amir Khodabande <amirdp8102@gmail.com>
2021-04-12 08:42:04 +0430

    Add test to check relationship between articles & tags

--- tests/Feature/CreateArticleTest.php
+++ tests/Feature/CreateArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Article;
+use App\Models\Tag;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -23,5 +24,32 @@ class CreateArticleTest extends TestCase
             ->assertStatus(201);
 
         $this->assertEquals(1, Article::count());
+    }
+
+    /** @test */
+    public function a_created_article_connect_to_tags_successfully()
+    {
+        $tags = Tag::factory(2)->create();
+
+        $article = [
+            'title' => 'article title',
+            'tags_id' => [1, 2]
+        ];
+
+        $this->post("/api/articles", $article)
+            ->assertStatus(201);
+
+        $this->assertEquals(1, Article::count());
+
+        $this->assertDatabaseHas('taggables', [
+            'tag_id' => '1',
+            'taggable_id' => '1',
+            'taggable_type' => 'App\Models\Article',
+        ]);
+        $this->assertDatabaseHas('taggables', [
+            'tag_id' => '2',
+            'taggable_id' => '1',
+            'taggable_type' => 'App\Models\Article',
+        ]);
     }
 }
