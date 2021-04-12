906092714bb39c69053894c10416f00eed615b04
Amir Khodabande <amirdp8102@gmail.com>
2021-04-12 08:31:32 +0430

    Add test to check relationship between products & tags

--- tests/Feature/CreateProductTest.php
+++ tests/Feature/CreateProductTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use App\Models\Tag;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -23,5 +24,32 @@ class CreateProductTest extends TestCase
             ->assertStatus(201);
 
         $this->assertEquals(1, Product::count());
+    }
+
+    /** @test */
+    public function a_created_product_connect_to_tags_successfully()
+    {
+        $tags = Tag::factory(2)->create();
+
+        $product = [
+            'name' => 'product name',
+            'tags_id' => [1, 2]
+        ];
+
+        $this->post("/api/products", $product)
+            ->assertStatus(201);
+
+        $this->assertEquals(1, Product::count());
+
+        $this->assertDatabaseHas('taggables', [
+            'tag_id' => '1',
+            'taggable_id' => '1',
+            'taggable_type' => 'App\Models\Product',
+        ]);
+        $this->assertDatabaseHas('taggables', [
+            'tag_id' => '2',
+            'taggable_id' => '1',
+            'taggable_type' => 'App\Models\Product',
+        ]);
     }
 }
